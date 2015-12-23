c553a7e930f16289f2427c7a1642481c5073f9dc
Thiago Heitling <thiago.heitling@gmail.com>
2015-12-23 14:53:47 -0800

    Initial Commit

---
+++ FuzzBizzNezz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FuzzBizzNezz
+//
+//  Created by Thiago Heitling on 2015-12-23.
+//  Copyright © 2015 Thiago Heitling. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
