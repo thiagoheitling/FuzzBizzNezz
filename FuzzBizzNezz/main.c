6c2bf2cf670d04ae8f3fcef66f21c93dedaed7d0
Thiago Heitling <thiago.heitling@gmail.com>
2015-12-28 11:52:23 -0800

    implemented a function that scans three int

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -8,10 +8,11 @@
 
 #include <stdio.h>
 
-int main() {
+void fuzzBizzNezz() {
     int fuzz = 0;
     int bizz = 0;
     int nezz = 0;
+    
     while (fuzz <= 0) {
         printf("Please, enter a positive number: ");
         scanf("%i", &fuzz);
@@ -25,6 +26,7 @@ int main() {
         scanf("%i", &nezz);
     }
     printf("List of numbers from 1 to %i. Printed <Fuzz> for multiples of %i, <Bizz> for multiples of %i and <FuzzBizz> for multiples of both %i and %i: \n", nezz, fuzz, bizz, fuzz, bizz);
+    
     for (int i = 1; i <= nezz; i++) {
         if(i % fuzz == 0 && i % bizz == 0) {
             printf("FuzzBizz \n");
@@ -39,5 +41,9 @@ int main() {
             printf("%i \n", i);
         }
     }
+}
+
+int main() {
+    fuzzBizzNezz();
     return 0;
 }
